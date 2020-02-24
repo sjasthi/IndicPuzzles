f2491c89117e577ac1b70e3e2c22f8e6214df9ac
coleBurnham <54560774+coleBurnham@users.noreply.github.com>
2020-02-24 16:51:26 -0600

    added dummy pages for delete/create
    still need to  make changes, but added pages

---
+++ createBook.php
@@ -0,0 +1,91 @@
+<?php $page_title = 'Puzzles > Create Puzzle'; ?>
+<?php 
+    require 'bin/functions.php';
+    require 'db_configuration.php';
+    include('header.php'); 
+    $page="puzzles_list.php";   
+    verifyLogin($page); 
+
+?>
+<?php 
+    $mysqli = NEW MySQLi('localhost','root','','quiz_master');
+    $resultset = $mysqli->query("SELECT DISTINCT topic FROM topics ORDER BY topic ASC");   
+?>
+<link href="css/form.css" rel="stylesheet">
+<style>#title {text-align: center; color: darkgoldenrod;}</style>
+<div class="container">
+    <!--Check the CeremonyCreated and if Failed, display the error message-->
+    <?php
+    if(isset($_GET['createPuzzle'])){
+        if($_GET["createPuzzle"] == "fileRealFailed"){
+            echo '<br><h3 align="center" class="bg-danger">FAILURE - Your image is not real, Please Try Again!</h3>';
+        }
+    }
+    if(isset($_GET['createPuzzle'])){
+        if($_GET["createPuzzle"] == "answerFailed"){
+            echo '<br><h3 align="center" class="bg-danger">FAILURE - Your answer was not one of the choices, Please Try Again!</h3>';
+        }
+    }
+    if(isset($_GET['createPuzzle'])){
+        if($_GET["createPuzzle"] == "fileTypeFailed"){
+            echo '<br><h3 align="center" class="bg-danger">FAILURE - Your image is not a valid image type (jpg,jpeg,png,gif), Please Try Again!</h3>';
+        }
+    }
+    if(isset($_GET['createPuzzle'])){
+        if($_GET["createPuzzle"] == "fileExistFailed"){
+            echo '<br><h3 align="center" class="bg-danger">FAILURE - There is already a puzzle using that image, Please Try Again!</h3>';
+        }
+    }
+  
+    ?>
+    <form action="createThePuzzle.php" method="POST" enctype="multipart/form-data">
+        <br>
+        <h3 id="title">Create A Puzzle</h3> <br>
+        
+        <table>
+            <!-- Puzzle name -->
+            <tr>
+                <td style="width:100px">Puzzle name:</td>
+                <td><input type="text"  name="puzzleName" maxlength="50" size="50" required title="Please enter the name of the puzzle"></td>
+            </tr>
+            <!-- Creator name -->
+            <tr>
+                <td style="width:100px">Creator:</td>
+                <td><input type="text"  name="creatorName" maxlength="50" size="50" required title="Please enter the creator's name"></td>
+            </tr>
+            <!-- Author name -->
+            <tr>
+                <td style="width:100px">Author:</td>
+                <td><input type="text"  name="authorName" maxlength="50" size="50" required title="Please enter the author's name"></td>
+            </tr>
+            <!-- Book name -->
+            <tr>
+                <td style="width:100px">Book name:</td>
+                <td><input type="text"  name="bookName" maxlength="50" size="50" required title="Please enter the name of the book this puzzle will be in."></td>
+            </tr>
+            <!-- Puzzle -->
+            <tr>
+                <td style="width:100px">Puzzle:</td>
+                <td><input type="file" name="puzzleFileToUpload" id="puzzleFileToUpload" maxlength="50" size="50" title="Enter the puzzle"></td>
+            </tr>
+            <!-- Solution -->
+            <tr>
+                <td style="width:100px">Solution:</td>
+                <td><input type="file" name="solutionFileToUpload" id="solutionFileToUpload" maxlength="50" size="50" title="Enter the solution to the puzzle"></td>
+            </tr>
+            <!-- Notes -->
+            <tr>
+                <td style="width:100px">Notes:</td>
+                <td><input type="text"  name="notes" maxlength="50" size="50" required title="Please enter notes about the puzzle."></td>
+            </tr>
+        </table>
+
+        <br><br>
+        <div align="center" class="text-left">
+            <button type="submit" name="submit" class="btn btn-primary btn-md align-items-center">Create Puzzle</button>
+        </div>
+        <br> <br>
+
+    </form>
+</div>
+
---
+++ createTheBook.php
@@ -0,0 +1,90 @@
+<?php
+
+include_once 'db_configuration.php';
+
+    // Initialize variables        
+    echo "HERE";
+    $puzzleName = mysqli_real_escape_string($db,$_POST['puzzleName']);
+    $creatorName = mysqli_real_escape_string($db,$_POST['creatorName']);
+    $authorName = mysqli_real_escape_string($db,$_POST['authorName']);
+    $bookName = mysqli_real_escape_string($db,$_POST['bookName']);
+    $puzzleFileToUploadName = basename($_FILES["puzzleFileToUpload"]["name"]);
+    $solutionFileToUploadName = basename($_FILES["solutionFileToUpload"]["name"]);
+    $notes = mysqli_real_escape_string($db,$_POST['notes']);
+    $validate = true;    
+    
+    if($validate){
+        // ====================== Puzzle image =====================
+        $target_dir = "Images/puzzle_images/";
+        $target_file = $target_dir . basename($_FILES["puzzleFileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["puzzleFileToUpload"]["tmp_name"]);
+            if($check !== false) {
+                $uploadOk = 1;
+            } else {
+                header('location: createPuzzle.php?createPuzzle=fileRealFailed');
+                $uploadOk = 0;
+            }
+        }
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            header('location: createPuzzle.php?createPuzzle=fileExistFailed');
+            $uploadOk = 0;
+        }
+        
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+            header('location: createPuzzle.php?createPuzzle=fileTypeFailed');
+            $uploadOk = 0;
+        }
+
+        // ====================== Solution image =====================
+
+        $target_dir = "Images/solution_images/";
+        $target_file1 = $target_dir . basename($_FILES["solutionFileToUpload"]["name"]);        
+        $imageFileType = strtolower(pathinfo($target_file1,PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["solutionFileToUpload"]["tmp_name"]);
+            if($check !== false) {
+                $uploadOk = 1;
+            } else {
+                header('location: createPuzzle.php?createPuzzle=fileRealFailed');
+                $uploadOk = 0;
+            }
+        }
+        // Check if file already exists
+        if (file_exists($target_file1)) {
+            header('location: createPuzzle.php?createPuzzle=fileExistFailed');
+            $uploadOk = 0;
+        }
+        
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+            header('location: createPuzzle.php?createPuzzle=fileTypeFailed');
+            $uploadOk = 0;
+        }
+
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {            
+
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["puzzleFileToUpload"]["tmp_name"], $target_file) && move_uploaded_file($_FILES["solutionFileToUpload"]["tmp_name"], $target_file1)) {
+                
+                $sql = "INSERT INTO gpuzzles(puzzle_name, creator_name, author_name, book_name, puzzle_image, solution_image, notes)
+                VALUES ('$puzzleName','$creatorName','$authorName','$bookName','$puzzleFileToUploadName','$solutionFileToUploadName','$notes')
+                ";
+
+                mysqli_query($db, $sql);
+                header('location: puzzles_list.php?createPuzzle=Success');
+                }
+            }
+        }else{
+            header('location: createPuzzle.php?createPuzzle=answerFailed'); 
+    }        

f2492b262806e751b51db778b18733b60ec4d548
Cole Burnham <pi0863jx@metrostate.edu>
2020-03-03 19:52:51 -0600

    added nav to createBook
    added nav bar to the create book page

--- createBook.php
+++ createBook.php
@@ -2,7 +2,7 @@
 <?php 
     require 'bin/functions.php';
     require 'db_configuration.php';
-    include('header.php'); 
+    include('nav.php'); 
     $page="puzzles_list.php";   
    // verifyLogin($page); 
 

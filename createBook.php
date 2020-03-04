36eb9fb9828ebee88bf6e6b7705fb6eb019cae5b
Cole Burnham <pi0863jx@metrostate.edu>
2020-03-03 21:10:45 -0600

    Update createBook.php

--- createBook.php
+++ createBook.php
@@ -57,16 +57,7 @@
                 <td style="width:100px">Book name:</td>
                 <td><input type="text"  name="bookName" maxlength="50" size="50" required title="Please enter the name of the book this puzzle will be in."></td>
             </tr>
-            <!-- Puzzle -->
-            <tr>
-                <td style="width:100px">Puzzle:</td>
-                <td><input type="file" name="puzzleFileToUpload" id="puzzleFileToUpload" maxlength="50" size="50" title="Enter the puzzle"></td>
-            </tr>
-            <!-- Solution -->
-            <tr>
-                <td style="width:100px">Solution:</td>
-                <td><input type="file" name="solutionFileToUpload" id="solutionFileToUpload" maxlength="50" size="50" title="Enter the solution to the puzzle"></td>
-            </tr>
+
 
         </table>
 

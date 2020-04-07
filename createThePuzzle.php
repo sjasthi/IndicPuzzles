183b7454963413f4a96c64b64aaae69e0db5b163
Cole Burnham <pi0863jx@metrostate.edu>
2020-04-07 13:57:41 -0500

    mass upload
    getting weird errors on file count / allowed bytes

--- createThePuzzle.php
+++ createThePuzzle.php
@@ -1,5 +1,6 @@
 <?php
 
+
 include_once 'db_configuration.php';
 
     // Initialize variables        
--- createThePuzzlesMass.php
+++ createThePuzzlesMass.php
@@ -7,87 +7,7 @@
     //https://jsfiddle.net/kevalpadia/vk6Ldzae/?utm_source=website&utm_medium=embed&utm_campaign=vk6Ldzae
     //https://dcblog.dev/creating-an-image-gallery-from-a-folder-of-images-automatically
      ?>
-<form method="post" enctype="multipart/form-data" action="#">
-<br>
-        <h3 id="title">Create A Puzzle</h3> <br>
-        
-        <table>
-            <!-- Puzzle name -->
-            <tr>
-                <td style="width:400px">Puzzle name will be the name of the image</td>
-            </tr>
-            <!-- Creator name -->
-            <tr>
-                <td style="width:100px">Creator:</td>
-                <td><input type="text"  name="creatorName" maxlength="50" size="50" required title="Please enter the creator's name"></td>
-            </tr>
-            <!-- Author name -->
-            <tr>
-                <td style="width:100px">Author:</td>
-                <td><input type="text"  name="authorName" maxlength="50" size="50" required title="Please enter the author's name"></td>
-            </tr>
-            <!-- Book name 
-            <tr>
-                <td style="width:100px">Book name:</td>
-                <td><input type="select"  name="bookName" maxlength="50" size="50" required title="Please enter the name of the book this puzzle will be in."></td>
-        -->
-                <?php
 
-$conn = new mysqli('localhost', 'root', '', 'gpuzzles_db') 
-or die ('Cannot connect to db');
-
-    $result = $conn->query("select book_name from books");
-    
-    echo "<html>";
-    echo "<body>";
-    echo "<select name='bookd_name'>";
-
-    while ($row = $result->fetch_assoc()) {
-
-                  unset($id, $name);
-                  $id = $row['book_name'];
-                  $name = $row['book_name']; 
-                  echo '<option value="'.$id.'">'.$name.'</option>';
-                 
-}
-
-    echo "</select>";
-    echo "</body>";
-    echo "</html>";
-?>
-</tr>
-
-
-            <!-- Puzzle Old way of uploading mass images
-
-            <tr>
-                <td style="width:100px">Puzzle:</td>
-                <td><input type="file" name="puzzleFileToUpload[]" id="puzzleFileToUpload" maxlength="50" size="50" title="Enter the puzzle" multiple></td>
-            </tr>-->
-            <!-- Solution -->
-            <tr>
-                <td style="width:100px">Solution:</td>
-                <td><input type="file" name="solutionFileToUpload" id="solutionFileToUpload" maxlength="50" size="50" title="Enter the solution to the puzzle"></td>
-            </tr>
-            <!-- Notes -->
-            <tr>
-                <td style="width:100px">Notes:</td>
-                <td><input type="text"  name="notes" maxlength="50" size="50" required title="Please enter notes about the puzzle."></td>
-            </tr>
-        </table>
-
-        <br><br>
-        <div align="center" class="text-left">
-            <button type="submit" name="submit" class="btn btn-primary btn-md align-items-center">Create Puzzle</button>
-        </div>
-        <br> <br>
-
-    </form>
-</div>
-        Folder Name: <input type="text" name="foldername" /><br/>
-        Choose Directoryy:  <input type="file" name="files[]" id="files" multiple directory="" webkitdirectory="" mozdirectory=""><br/>
-    <input class="button" type="submit" value="Upload" name="upload" />
-</form>
 <?php
 
     $mysqli = NEW MySQLi('localhost','root','','gpuzzles_db');
@@ -105,14 +25,23 @@ if(isset($_POST['upload']))
 {
         if($_POST['foldername']!="")
         {
-                $foldername=$_POST['foldername'];
+                $foldername=$_POST['bookName'];
                 if(!is_dir($foldername))
                         mkdir($foldername);
-                foreach($_FILES['files']['name'] as $i=>$name)
+                foreach($_FILES['files']['name'] as $i=>$puzzleName)
+                {
+                
+                    $sql = "INSERT INTO gpuzzles(puzzle_name, creator_name, author_name, book_name, puzzle_image, solution_image, notes)
+                    VALUES ('$puzzleName','$creatorName','$authorName','$bookName','$puzzleFileToUploadName','$solutionFileToUploadName','$notes')
+                    ";
+    
+                    mysqli_query($db, $sql);
+                    header('location: puzzles_list.php?createPuzzle=Success');
+                    }
                 {
                         if(strlen($_FILES['files']['name'][$i]) > 1)
                         {
-                                move_uploaded_file($_FILES['files']['tmp_name'][$i],$foldername.'/'.$name);
+                                move_uploaded_file($_FILES['files']['tmp_name'][$i],$foldername.'/'.$puzzleName);
                         }
                 }
                 echo "Folder is uploaded successfully ..";
